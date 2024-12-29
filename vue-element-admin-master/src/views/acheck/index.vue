42527c470784d5d4f942aac39c0063493fd5c3ea
22wjLiu <wanyaba@outlook.com>
2024-12-30 00:35:16 +0800

    删除token验证

--- vue-element-admin-master/src/views/acheck/index.vue
+++ vue-element-admin-master/src/views/acheck/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="app-container">
     <div class="filter-container">
-  
+
       <el-button class="filter-item" style="margin-left: 10px;" type="primary" icon="el-icon-edit" @click="handleCreate">
         添加
       </el-button>
-     
+
     </div>
 
     <el-table
